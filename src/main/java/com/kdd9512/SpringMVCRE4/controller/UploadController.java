1afc12ce78e97588704a880e1b56acecc8d55203
kdd9512 <hyunwoo8805@gmail.com>
2022-09-11 23:04:56 +0900

    20220911 10th commit
    파일 업로드 처리 실습 요구사항.
    1) 업로드하는 파일 이름이 같으면 기존 파일이 사라짐. V 20220910
    2) 이미지 용량이 큰 경우 섬네일을 따로 생성해줘야. V 20220911
    3) 이미지 / 일반 파일 구분. V 20220911
    4) 업로드 파일 확장자, 최대용량 제한. V 20220909
    
    4) 업로드 할 시 파일의 종류를 구분하고 최대용량을
    제한 할 수 있도록 함수 구현(checkExtension).
    -> exe,sh,zip,alz,rar 불허.
    
    1) UploadController 에서 처리.
    - UUID 를 이용하여 같은 파일이 생성될 수 없도록 조치.
    - mkdirs 를 이용하여 업로드 성공 날짜에 맞춰 폴더경로를 생성 +
      파일 저장.
    - 업로드할 파일이 없는 경우에도 업로드가 실행되는 것을 방지하기 위해
      간단한 script 추가.
    
    2) 3) 이미지타입인가를 구분하고, 이미지인 경우 Thumbnail 을 생성.
    - pom.xml 에 thumbnailator 를 추가.
    - UploadController 에 업로드 파일이 이미지 타입인지를 구분하기 위한
      새 method checkImageType() 을 추가.
    - method 를 if문 처리하여 조건에 맞는 경우, Thumbnailator 를
      이용하여 섬네일 제작.

--- src/main/java/com/kdd9512/SpringMVCRE4/controller/UploadController.java
+++ src/main/java/com/kdd9512/SpringMVCRE4/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.kdd9512.SpringMVCRE4.controller;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -28,7 +32,7 @@ public class UploadController {
     }
 
     // 경로 생성을 위한 method
-    private String getFolder(){
+    private String getFolder() {
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
@@ -36,6 +40,20 @@ public class UploadController {
 
         return str.replace("-", File.separator);
 
+    }
+
+    // 이미지 파일인지 구분하기 위한 method
+    private boolean checkImageType(File file) {
+        try {
+            String contentType = Files.probeContentType(file.toPath());
+
+            return contentType.startsWith("image");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
     // POST 기능
@@ -69,6 +87,7 @@ public class UploadController {
         log.info("Update Ajax POST...........................");
         String uploadFolder = "C:\\JAVA\\galupload";
 
+        // 폴더 생성.
         File uploadPath = new File(uploadFolder, getFolder());
         log.info("Upload Folder : " + uploadPath);
 
@@ -76,6 +95,7 @@ public class UploadController {
             uploadPath.mkdirs();
         }
 
+        // 폴더 경로(월/일) 생성.
         for (MultipartFile multipartFile : uploadFile) {
 
             log.info("-----------------------------------------------------------");
@@ -92,19 +112,24 @@ public class UploadController {
 
             uploadFileName = uuid + "_" + uploadFileName;
 
-//            File saveFile = new File(uploadFolder, uploadFileName);
-            File saveFile = new File(uploadPath, uploadFileName);
-
+            // Thumbnail 생성.
             try {
+                File saveFile = new File(uploadPath, uploadFileName);
                 multipartFile.transferTo(saveFile);
+
+                // 업로드하는 파일이 이미지타입인지 체크
+                if (checkImageType(saveFile)) {
+                    FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "th_" + uploadFileName));
+                    Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+                    thumbnail.close();
+                }
             } catch (Exception e) {
-                log.error(e.getMessage());
+                e.printStackTrace();
             }
 
         }
 
     }
-
 
 
 }
