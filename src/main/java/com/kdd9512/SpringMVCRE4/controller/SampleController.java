8ccc30f2e99635d3dd2e3838236eec67f8bbc541
kdd9512 <hyunwoo8805@gmail.com>
2022-10-28 22:43:59 +0900

    20221028 56th commit
    * spring web security 적용 연습
    - servlet 에 security 활성화

--- src/main/java/com/kdd9512/SpringMVCRE4/controller/SampleController.java
+++ src/main/java/com/kdd9512/SpringMVCRE4/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.kdd9512.SpringMVCRE4.controller;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,19 @@ public class SampleController {
     @GetMapping("/customLogout")
     public void logoutGet() {
         log.info("custom logout");
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
+    @GetMapping("/annoMember")
+    public void doMember2(){
+
+        log.info("login annotation member");
+    }
+
+    @Secured({"ROLE_ADMIN"})
+    @GetMapping("/annoAdmin")
+    public void doAdmin2(){
+        log.info("admin annotation only");
     }
 
 
